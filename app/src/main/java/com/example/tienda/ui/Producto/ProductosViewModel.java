a4af189258548a716f7ddccbf895a59036901520
Donnie9897 <49081151+Donnie9897@users.noreply.github.com>
2022-12-05 14:07:05 -0400

    Fragmentos del Nav Menu (falta el optionsMenu y el login)

--- app/src/main/java/com/example/tienda/ui/Producto/ProductosViewModel.java
+++ app/src/main/java/com/example/tienda/ui/Producto/ProductosViewModel.java
@@ -1,7 +1,18 @@
 package com.example.tienda.ui.Producto;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class ProductosViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+    private final MutableLiveData<String> mText;
+
+    public ProductosViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is Productos fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
 }
