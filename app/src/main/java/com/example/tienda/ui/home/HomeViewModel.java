d00abb42f733835b98ca71d2a3ade8a652af0c6b
Donnie9897 <49081151+Donnie9897@users.noreply.github.com>
2022-12-05 15:36:45 -0400

    Fragmentos del Nav Menu (falta el optionsMenu y el login)

--- app/src/main/java/com/example/tienda/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/tienda/ui/home/HomeViewModel.java
@@ -4,13 +4,19 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.tienda.Product;
+
 public class HomeViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
+    private Product producto;
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+    //    mText.setValue("Teclado");
+
+        producto = new Product("Teclado","Un teclado RGB GAMER","120$");
+
     }
 
     public LiveData<String> getText() {
